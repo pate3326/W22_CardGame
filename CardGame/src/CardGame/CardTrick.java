d7d623eacc0d8d0ab9860db89973e6e756278433
pate3326 <98785334+pate3326@users.noreply.github.com>
2022-02-08 16:52:44 -0500

    First commit

---
+++ CardGame/src/CardGame/CardTrick.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CardGame;
+
+/**
+ *
+ * @author Pate3326
+ */
+public class CardTrick {
+
+public static void main(String[] args) {
+  
+CardGame[] magicHand = new CardGame[7];
+
+for (int i = 0; i < magicHand.length; i++) {
+CardGame c = new CardGame();
+}
+}
+
+}
+
